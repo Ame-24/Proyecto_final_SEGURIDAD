149eea574e0ad125715ffb8d91bebe7f457519f7
gevidic <genesisuvidia5@gmail.com>
2025-06-29 20:35:17 -0500

    La clase SistemaControl se encarga de organizar y manejar toda la información, guardando listas de propietarios, invitados, espacios comunes, reservas y registros de accesos, permitiendo así controlar quién vive, quién visita, qué espacios se usan y cuándo, todo en un solo lugar.

--- PROYECTO_P3/src/sistema/SistemaControl.java
+++ PROYECTO_P3/src/sistema/SistemaControl.java
@@ -3,3 +3,10 @@ package sistema;
 import modelo.*;
 import java.io.*;
 import java.util.*;
+
+public class SistemaControl {
+    private List<Propietario> propietarios = new ArrayList<>();
+    private List<Invitado> invitados = new ArrayList<>();
+    private List<EspacioComun> espacios = new ArrayList<>();
+    private List<ReservaEspacio> reservas = new ArrayList<>();
+    private List<RegistroAcceso> registros = new ArrayList<>();
